e53b50efba5c5d6e7da1e834884e41c713d44d92
Darxxn <stonelai11@gmail.com>
2025-03-29 21:38:31 -0600

    changed to online database rather than manual db

--- server/main.py
+++ server/main.py
@@ -14,9 +14,9 @@ from pydantic import BaseModel
 load_dotenv()
 
 # PostgreSQL connection
-SQLALCHEMY_DATABASE_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
