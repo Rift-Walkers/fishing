c8aba7fd37b347e9e49bf055d3995f1c8c8cb99c
Darxxn <stonelai11@gmail.com>
2025-03-30 00:38:47 -0600

    extra http

--- server/main.py
+++ server/main.py
@@ -44,7 +44,7 @@ app = FastAPI()
 
 # CORS configuration
 origins = [
-    "http://http://127.0.0.1:5500",
+    "http://127.0.0.1:5500",
     "http://localhost:5500",
     # Add your production URL(s) here too if you deploy the client elsewhere
 ]
