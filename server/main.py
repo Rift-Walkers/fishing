b23b5fdd71c9444aa63e94486e3ca540092aec25
Darxxn <stonelai11@gmail.com>
2025-03-30 00:42:06 -0600

    changes for railway postgresql

--- server/main.py
+++ server/main.py
@@ -44,9 +44,7 @@ app = FastAPI()
 
 # CORS configuration
 origins = [
-    "http://127.0.0.1:5500",
-    "http://localhost:5500",
-    # Add your production URL(s) here too if you deploy the client elsewhere
+    "127.0.0.1:5500/client/auth.html",
 ]
 
 app.add_middleware(
