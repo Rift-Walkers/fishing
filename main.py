2846d275814e5df874ec8ccf9b1840c8683069a2
Darxxn <stonelai11@gmail.com>
2025-03-30 19:07:41 -0600

    changing creds to false

--- main.py
+++ main.py
@@ -44,13 +44,14 @@ app = FastAPI()
 
 # CORS configuration
 origins = [
-    "http://127.0.0.1:5500/"
+    "http://127.0.0.1:5500",
+    "http://localhost:5500"
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
